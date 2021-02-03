abe12e8b4742fd66a70445792dcb2f00fce6f159
Vincent Guizot <vincentguizot128@gmail.com>
2021-02-03 17:39:24 +0700

    soal week 1

---
+++ soal-tambahan/w1-looping.js
@@ -0,0 +1,46 @@
+/**
+ * 1. Print angka Kelipatan 3, 5, 3 dan 5
+ * 
+ * Input = 15
+ * Output =
+ * 1
+ * 2
+ * 3 adalah Kelipatan 3
+ * 4
+ * 5 adalah Kelipatan 5
+ * 6 adalah Kelipatan 3
+ * 7
+ * 8
+ * 9 adalah Kelipatan 3
+ * 10 adalah Kelipatan 5
+ * 11
+ * 12 adalah Kelipatan 3
+ * 13
+ * 14
+ * 15 adalah Kelipatan 3 dan 5
+ */
+
+/** 
+ * 2. Print hasil faktorial
+ * Defini faktorial = n! -> n.(n-1).(n-2)...1
+ * 
+ * Contoh : 
+ * 5! = 5.4.3.2.1
+ * 
+ * Input = 5 // 120
+ * Input = 10 // 
+ * */  
+
+ /**
+  * 3. Rupiah Formatter
+  * 
+  * Input = 1000000
+  * Output = Rp. 1.000.000
+  * 
+  * Input = 15350500
+  * Output = Rp. 15.350.500
+  * */ 
+ 
+ /**
+  * 
+  *  */ 
