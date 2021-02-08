1dc5a5342f5ab64a7b42efe2b5c151bb1c256bba
Vincent Guizot <vincentguizot128@gmail.com>
2021-02-08 16:18:50 +0700

    array

---
+++ rangkuman/array.js
@@ -0,0 +1,32 @@
+// Array 
+// Variable yang memiliki banyak value dan index yang berurut
+
+
+let numbers = [1, 2, 5, 7, 9]
+
+let people = ['agus', 'haryo', 'farhan']
+
+let various = [10, 'angka', true, null, undefined, '105']
+
+
+// console.log(numbers[2])
+// console.log(people[0])
+// console.log(people[0][3])
+
+// console.log(various.length)
+
+// console.log(numbers[numbers.length - 1])
+
+// .push()
+numbers.push(10,12)
+numbers.push(15,17,19)
+console.log(numbers)
+
+numbers.pop()
+console.log(numbers)
+
+numbers.unshift(-1,0)
+console.log(numbers)
+
+numbers.shift()
+console.log(numbers)
