9ccb8185341a499290d0e3433f3eef2fc9ec47ca
Vincent Guizot <vincentguizot128@gmail.com>
2021-02-17 15:33:29 +0700

    array of objects

---
+++ rangkuman/dist/arrayofobject.dev.js
@@ -0,0 +1,76 @@
+"use strict";
+
+// Array of objects
+// students[2].isGraduate = false
+// console.log(students)
+// for (let i = 0; i < students.length; i++) {
+//     // for(let key in students[i]){
+//     //     console.log(key)
+//     // }
+//     console.log(students[i].name)
+//     console.log(students[i].gpa)
+//     console.log(students[i].isGraduate)
+// }
+// Buat function -> merubah object menjadi array multidimensi
+// algoritma
+
+/**
+ * 1. Looping sesuai array of objects
+ * 2. Looping object menggunnakan for in 
+ * 3. Buat variable temp untuk menampung value dr object
+ * 4. Buat variabel result untuk menampung array dr temp
+ * 5. Push value key dr object ke dalam temp
+ * 6. Push temp ke dalam result
+ * 7. Return result
+ */
+// let students = [
+//     {
+//         name: "Vincent",
+//         gpa: 3.21,
+//         isGraduate: true
+//     },
+//     {
+//         name: "Stella",
+//         gpa: 3.05,
+//         isGraduate: true
+//     },
+//     {
+//         name: "Fendy",
+//         gpa: 3.75,
+//         isGraduate: true
+//     }
+// ]
+function convertToArray(obj) {
+  // code here 
+  var temp = [];
+  var result = [];
+
+  for (var i = 0; i < obj.length; i++) {
+    temp = [];
+
+    for (var key in obj[i]) {
+      temp.push(obj[i][key]);
+    }
+
+    result.push(temp);
+  }
+
+  return result;
+}
+
+var resultStudents = [["Vincent", 3.21, true], ["Stella", 3.05, true], ["Fendy", 3.75, true]]; // console.log(convertToArray(students))
+
+var student = {
+  name: "Vincent",
+  gpa: 3.21,
+  isGraduate: true,
+  eat: function eat() {
+    return "Saya Makan";
+  },
+  drink: function drink(drinks) {
+    return "Saya minum ".concat(drinks);
+  }
+};
+console.log(student.name);
+console.log(student.eat());
+console.log(student.drink("Kopi Susu Gula Aren Yes"));
---
+++ rangkuman/latihan-todo.js
@@ -0,0 +1,126 @@
+// Buat todo apps
+
+/**
+ * 5 Function : addTodo(),getTodo(),deleteTodo(),updateTodo(),changeStatus()
+ * 
+ */
+
+// getTodo(todos)
+/**
+   1. Belajar js [X]
+   2. Makan bersama [ ]
+
+ */
+
+// addTodo(tasks)
+/*
+    Task adalah string, 
+    addTodo("Minum bersama")
+
+    Output :
+    "Minum bersama" telah di masukkan!
+
+    sehingga dalam todos :
+    [
+        {
+            id: 1,
+            task: "Belajar Js",
+            status: true
+        },
+        {
+            id: 2,
+            task: "Makan bersama",
+            status: false
+        },
+        {
+            id : 3,
+            task : "Minum bersama",
+            status : false
+        }
+    ]
+*/
+
+// deleteTodo(id)
+/*
+    Menghapus lewat id
+*/
+
+let todos = [
+    {
+        id: 1,
+        task: "Belajar Js",
+        status: true
+    },
+    {
+        id: 2,
+        task: "Makan bersama",
+        status: false
+    }
+]
+
+function getTodo(todos) {
+    for (let i = 0; i < todos.length; i++) {
+        // Destructuring Object
+        const { id, task, status } = todos[i];
+
+        if (status) {
+            console.log(`[X] ${id}. ${task} `)
+        } else {
+            console.log(`[ ] ${id}. ${task} `)
+        }
+    }
+}
+
+function addTodo(task) {
+    let id = todos[todos.length - 1].id + 1
+    let status = false
+
+    todos.push({
+        id,
+        task,
+        status
+    })
+    console.log(`"${task}" telah di masukkan!`)
+}
+
+function deleteTodo(id) {
+    let temp = []
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id !== id) {
+            temp.push(todos[i])
+        }
+    }
+    todos = temp
+    console.log(`Id no ${id} telah di hapus!`)
+}
+
+function updateTodo(id, task) {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === id) {
+            todos[i].task = task
+        }
+    }
+    console.log(`Id no ${id} telah di ganti!`)
+}
+
+function changeStatus(id) {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === id) {
+            todos[i].status = !todos[i].status
+        }
+    }
+    console.log(`Id no ${id} telah di ganti statusnya!`)
+}
+
+addTodo("Minum bersama")
+addTodo("Maen genshin impact")
+addTodo("Lanjutin stardew valley")
+getTodo(todos)
+deleteTodo(3)
+deleteTodo(5)
+getTodo(todos)
+updateTodo(1, "Belajar array of objects seruuu!")
+getTodo(todos)
+changeStatus(2)
+changeStatus(4)
+getTodo(todos)
