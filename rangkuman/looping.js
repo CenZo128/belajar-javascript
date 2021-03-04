4d309c540e4fbbc049349a2a1bda6292c169c624
Vincent Guizot <vincentguizot128@gmail.com>
2021-03-04 16:58:55 +0700

    loop

--- rangkuman/looping.js
+++ rangkuman/looping.js
@@ -36,20 +36,101 @@
 // }
 // console.log(`Jumlah huruf a : ${flagA}`);
 
-var kodeProduksi = '123/ABC/XYZ/';
-var flag = 0;
+// var kodeProduksi = '123/ABC/XYZ/';
+// var flag = 0;
+// /**
+//  * Jika lebih atau kurang dari 2 maka INVALID
+//  * Jika sama dengan 2 maka VALID
+//  */
+// for(var i =0 ; i < kodeProduksi.length; i++){
+//     if(kodeProduksi[i] === '/'){
+//         flag += 1;
+//     }
+// }
+// console.log(flag)
+// if(flag === 2){
+//     console.log("VALID")
+// }else {
+//     console.log("INVALID")
+// }
+
+// let nama = "Jennie"
+// let flagE = false;
+// let countE = 0;
+// let countN = 0;
+
+// for (let i = 0; i < nama.length; i++) {
+//     if (nama[i] === 'e') {
+//         // flagE = true;
+//         countE++
+//     } else if (nama[i] === 'n') {
+//         countN++
+//     }
+//     // console.log(nama[i])
+// }
+
+
+// let i = 0;
+// while (i < nama.length) {
+//     if (nama[i] === 'e') {
+//         countE++;
+//     } else if (nama[i] === 'n') {
+//         countN++
+//     }
+//     i++
+// }
+// console.log("Huruf E ada : " + countE)
+// console.log("Huruf N ada : " + countN)
+
+// let numbers = '515278879701'
+// // let flag7 = false;
+
+// for(let i = 0; i < numbers.length; i++){
+//     // console.log(+numbers[i])
+//     if(+numbers[i] === 7){
+//         // flag7 = true;
+
+//         // if(flag7){
+//         //     break
+//         // }else{
+//         //     continue
+//         // }
+//         break;
+//     }else {
+//         console.log(+numbers[i])
+//     }
+// }
+
+let jojo = "jotaro, chikara wo misete kure!!";
+
+// Result
+// Jotaro,
+// Chikara
+// Wo
+// Misete
+// Kure!!
+
+// algoritma
 /**
- * Jika lebih atau kurang dari 2 maka INVALID
- * Jika sama dengan 2 maka VALID
+ * 1. Looping dari variable jojo
+ * 2. BUat variable temp
+ * 3. Jika ketemu selain spasi, tambahkan karakter ke temp
+ * 4. Jika bertemu spasi, tampilkan temp, lalu reset temp
+ * 5. Jika bertemu index terakhir variable jojo, tampilkan temp, lalu reset temp
  */
-for(var i =0 ; i < kodeProduksi.length; i++){
-    if(kodeProduksi[i] === '/'){
-        flag += 1;
+
+let temp = ''
+for(let i = 0; i < jojo.length; i++){
+    // console.log(jojo[i]);
+    if(temp === ''){
+        temp += jojo[i].toUpperCase()
+    }else {
+        temp += jojo[i]
+    }
+
+    if(jojo[i] === " "){
+        console.log(temp)
+        temp = ''
     }
 }
-console.log(flag)
-if(flag === 2){
-    console.log("VALID")
-}else {
-    console.log("INVALID")
-}
+console.log(temp);
